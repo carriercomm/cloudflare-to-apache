e2ed0853c5a5192b1facbdaba942563d897e2506
Lloyd Watkin <lloyd.watkin@surevine.com>
2015-01-06 15:44:02 +0000

    Use new test file

---
+++ test/test.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+node index < test/input.log > test/output.log && if [[ -n $(diff test/output.log test/correct-output.log) ]]; then echo 'Fail'; exit 1; else echo 'Pass'; exit 0; fi;
