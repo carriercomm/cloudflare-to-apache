6e7c262d75684dd4ae355bbf77e486905a6e0d99
Lloyd Watkin <lloyd.watkin@surevine.com>
2015-01-06 15:47:27 +0000

    More verbose fail

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-node index < test/input.log > test/output.log && if [[ -n $(diff test/output.log test/correct-output.log) ]]; then echo 'Fail'; exit 1; else echo 'Pass'; exit 0; fi;
+node index < test/input.log > test/output.log && if [[ -n $(diff test/output.log test/correct-output.log) ]]; 
+then
+  echo 'Fail';
+  diff test/output.log test/correct-output.log;
+  exit 1;
+else 
+  echo 'Pass';
+  exit 0;
+fi;
