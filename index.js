4d469dbd9b9b0070ce517d639357a92a12f05dbc
Lloyd Watkin <lloyd.watkin@surevine.com>
2015-01-06 11:46:34 +0000

    Fix indentation

--- index.js
+++ index.js
@@ -42,35 +42,33 @@ var writeApacheFormat = function(transformed) {
 }
 
 var transformRecord = function(record) {
-    var transformed = {}
-    var initialPart = record.split(' ', 3)
-    if (initialPart.length < 3) {
-      return
-    }
-    transformed.host = initialPart[0]
-    transformed.ip = initialPart[1]
-    transformed.date = new Date(parseInt(initialPart[2]) * 1000)
-    transformed.rawTimestamp = parseInt(initialPart[2])
-    var nextPart =  /^"(.*)" ([0-9]*) ([0-9]*) "(.*)" "(.*)" "(.*)"$/
-      .exec(record.substring(initialPart.join(' ').length + 1))
+  var transformed = {}
+  var initialPart = record.split(' ', 3)
+  if (initialPart.length < 3) {
+    return
+  }
+  transformed.host = initialPart[0]
+  transformed.ip = initialPart[1]
+  transformed.date = new Date(parseInt(initialPart[2]) * 1000)
+  transformed.rawTimestamp = parseInt(initialPart[2])
+  var nextPart =  /^"(.*)" ([0-9]*) ([0-9]*) "(.*)" "(.*)" "(.*)"$/
+    .exec(record.substring(initialPart.join(' ').length + 1))
 
-    if (nextPart) {
-      transformed.request = nextPart[1]
-      transformed.responseCode = parseInt(nextPart[2])
-      transformed.responseTime = parseInt(nextPart[3])
-      transformed.userAgent = nextPart[4]
-      transformed.bot = nextPart[5]
-      transformed.referrer = nextPart[6]
-      nextPart = null
-      initialPart = null
-    }
-    return writeApacheFormat(transformed)
+  transformed.request = nextPart[1]
+  transformed.responseCode = parseInt(nextPart[2])
+  transformed.responseTime = parseInt(nextPart[3])
+  transformed.userAgent = nextPart[4]
+  transformed.bot = nextPart[5]
+  transformed.referrer = nextPart[6]
+  nextPart = null
+  initialPart = null
+  return writeApacheFormat(transformed)
 }
 
 var parser = new Transform();
 parser._transform = function(records, encoding, done) {
   records.toString('utf8').split('\n').forEach(function(record) {
-      this.push(transformRecord(record))
+    this.push(transformRecord(record))
   }, this)
   done()
 }
