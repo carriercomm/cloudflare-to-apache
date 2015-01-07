db232b41a4667665306a98b77a8eeec7186054c5
Lloyd Watkin <lloyd.watkin@surevine.com>
2015-01-07 12:26:20 +0000

    Interpret new format and convert

--- index.js
+++ index.js
@@ -15,18 +15,17 @@ var convertDataFormat = function(record) {
       log('Appending hangover, record now:\n' + record)
       hangover = null
   }
-  var initialPart = record.split(' ', 3)
-  if (initialPart.length < 3) {
+  var initialPart = record.split(' ', 5)
+  if (initialPart.length < 5) {
     hangover = record
-    throw new Error('Initial part not 3 records long! ' + initialPart.length)
+    throw new Error('Initial part not 5 records long! ' + initialPart.length)
   }
   transformed.host = initialPart[0]
   transformed.ip = initialPart[1]
-  transformed.date = new Date(parseInt(initialPart[2]) * 1000)
-  transformed.rawTimestamp = parseInt(initialPart[2])
-  var nextPart =  /^"(.*)" ([0-9]*) ([0-9]*) "(.*)" "(.*)" "(.*)"$/
+  transformed.date = new Date(Date.parse(initialPart[4]))
+  transformed.rawTimestamp = initialPart[4]
+  var nextPart =  /^"(.*)" ([0-9]*) ([0-9]*) "(.*)" "(.*)" (.*)$/
     .exec(record.substring(initialPart.join(' ').length + 1))
-
   if (!nextPart) {
     hangover = record
     throw new Error('Could not transform record: ' + record)
@@ -34,9 +33,9 @@ var convertDataFormat = function(record) {
   transformed.request = nextPart[1]
   transformed.responseCode = parseInt(nextPart[2])
   transformed.responseTime = parseInt(nextPart[3])
-  transformed.userAgent = nextPart[4]
-  transformed.bot = nextPart[5]
-  transformed.referrer = nextPart[6]
+  transformed.referrer = nextPart[4]
+  transformed.userAgent = nextPart[5]
+  transformed.bot = nextPart[6]
   nextPart = null
   initialPart = null
   return transformed
@@ -79,13 +78,13 @@ if (argv.f && ('old' === argv.f.toLowerCase())) {
 }
 
 var getFormattedDate = function(date) {
-    var months = [ 0, 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dev' ]
+    var months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dev' ]
     var month = months[date.getMonth()]
     /* Assuming a UTC timestamp in logs */
     var timezone = '+0000'
     
     return [
-      date.getDate(),
+      (date.getDate() < 10) ? '0' + date.getDate() : date.getDate(),
       '/',
       month,
       '/',
